441d0a9501b037b3b099d0dbb3364b4b3481f136
Saif22 <saif.trabelsi33@gmail.com>
2018-07-20 20:47:13 +0200

    My Django app, first commit

---
+++ App_project_venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\pc\desktop\app_project_folder\app_project_venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
